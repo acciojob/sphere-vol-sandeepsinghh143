c8ffc41df5ade24de20db76f5c151566386e032c
Sandeep Singh <115461230+sandeepsinghh143@users.noreply.github.com>
2023-06-05 14:04:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-function volume_sphere() {
+function volume_sphere(event) {
     //Write your code here
+	event.preventDefault();
 	const radius=document.getElementById("radius").value;
-	const volume=(4/3*Math.PI *radius**3);
+	const volume=(4/3*Math.PI * radius**3);
 	document.getElementById("volume").value=volume.toFixed(4);
   
 } 
